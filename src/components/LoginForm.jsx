c223563a156d410d2c8b44bc62939675ad677874
amiola <amiolastyle@gmail.com>
2022-04-18 16:37:17 +0100

    connect netlify

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -39,7 +39,7 @@ const LoginForm = () => {
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Chatty Messaging App</h1>
+        <h1 className="title">Chatty Messaging Application</h1>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
